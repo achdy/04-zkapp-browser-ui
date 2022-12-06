b09c0c8b4c7224a41f984c5c0d2bffbe2a59a7ac
achdy <achdy.mail@gmail.com>
2022-12-06 21:47:18 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-82e501e10130bd2a.js
+++ _next/static/chunks/webpack-82e501e10130bd2a.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "b0b45ac18d506401" + ".css";
+/******/ 			return "static/css/" + "c62ff9a7f23f5e1a" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
